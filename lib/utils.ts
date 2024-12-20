7310438d48e87920e7097a1c0bafe1eb29dcc6b6
Renganathan21 <114427016+Renganathan21@users.noreply.github.com>
2024-12-20 21:33:13 +0530

    update commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,39 +1,167 @@
-import mongoose, { Mongoose } from 'mongoose';
+/* eslint-disable prefer-const */
+import { type ClassValue, clsx } from "clsx";
+import qs from "qs";
+import { twMerge } from "tailwind-merge";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+import { aspectRatioOptions } from "@/constants";
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
 
-// Declare a specific type for the global cache instead of using `any`
-declare global {
-  var mongooseCache: MongooseConnection | undefined;
+// ERROR HANDLER
+export const handleError = (error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message);
+    throw new Error(`Error: ${error.message}`);
+  } else if (typeof error === "string") {
+    console.error(error);
+    throw new Error(`Error: ${error}`);
+  } else {
+    console.error(error);
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+  }
+};
+
+// PLACEHOLDER LOADER - while image is transforming
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+  shimmer(1000, 1000)
+)}`;
+
+// FORM URL QUERY
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: {
+  searchParams: URLSearchParams;
+  key: string;
+  value: string | number | null;
+}) => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+  return `${window.location.pathname}?${qs.stringify(params, {
+    skipNulls: true,
+  })}`;
+};
+
+// REMOVE KEY FROM QUERY
+export function removeKeysFromQuery({
+  searchParams,
+  keysToRemove,
+}: {
+  searchParams: URLSearchParams;
+  keysToRemove: string[];
+}) {
+  const currentUrl = qs.parse(searchParams.toString());
+
+  keysToRemove.forEach((key) => {
+    delete currentUrl[key];
+  });
+
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key]
+  );
+
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
 }
 
-// Use `const` since `cached` is not reassigned
-const cached: MongooseConnection = globalThis.mongooseCache || { conn: null, promise: null };
+// DEBOUNCE
+export const debounce = (
+  func: (...args: unknown[]) => void,
+  delay: number
+) => {
+  let timeoutId: NodeJS.Timeout | null;
+  return (...args: unknown[]) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
 
-if (!globalThis.mongooseCache) {
-  globalThis.mongooseCache = cached;
-}
+// GET IMAGE SIZE
+export type AspectRatioKey = keyof typeof aspectRatioOptions;
+export const getImageSize = (
+  type: string,
+  image: { aspectRatio?: AspectRatioKey; width?: number; height?: number },
+  dimension: "width" | "height"
+): number => {
+  if (type === "fill") {
+    return (
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      1000
+    );
+  }
+  return image?.[dimension] || 1000;
+};
 
-export const connectToDatabase = async (): Promise<Mongoose> => {
-  if (cached.conn) return cached.conn;
-
-  if (!MONGODB_URL) {
-    throw new Error('Missing MONGODB_URL');
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
   }
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'dreamflux',
-      bufferCommands: false,
-    });
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
 
-  cached.conn = await cached.promise;
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
 
-  return cached.conn;
+// DEEP MERGE OBJECTS
+export const deepMergeObjects = (
+  obj1: Record<string, unknown>,
+  obj2: Record<string, unknown>
+): Record<string, unknown> => {
+  if (obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
+
+  const output: Record<string, unknown> = { ...obj2 };
+
+  for (const key in obj1) {
+    if (Object.prototype.hasOwnProperty.call(obj1, key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(
+          obj1[key] as Record<string, unknown>,
+          obj2[key] as Record<string, unknown>
+        );
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
+
+  return output;
 };
